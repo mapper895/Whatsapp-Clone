c7be80b81915c85e40110179aef88559702b2659
mapper895 <mapper.895.mp@gmail.com>
2021-03-05 14:24:12 -0600

    Login

--- src/App.js
+++ src/App.js
@@ -2,23 +2,31 @@ import "./App.css";
 import Sidebar from "./Sidebar";
 import Chat from "./Chat";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { useState } from "react";
+import Login from "./Login";
 
 function App() {
+  const [user, setUser] = useState(null);
+
   return (
     <div className="app">
-      <div className="app__body">
-        <Router>
-          <Sidebar />
-          <Switch>
-            <Route path="/rooms/:roomId">
-              <Chat />
-            </Route>
-            <Route path="/">
-              <Chat />
-            </Route>
-          </Switch>
-        </Router>
-      </div>
+      {!user ? (
+        <Login />
+      ) : (
+        <div className="app__body">
+          <Router>
+            <Sidebar />
+            <Switch>
+              <Route path="/rooms/:roomId">
+                <Chat />
+              </Route>
+              <Route path="/">
+                <Chat />
+              </Route>
+            </Switch>
+          </Router>
+        </div>
+      )}
     </div>
   );
 }
